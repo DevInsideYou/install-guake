12223e12703aaf4f99143cce76e40fe4f4713b23
Vladyslav Pekker <agilesteel@gmail.com>
2019-10-18 22:52:36 +0200

    adds desktop entries

--- install-guake.sh
+++ install-guake.sh
@@ -12,5 +12,13 @@ sudo apt install -y python3-pip
 # install guake
 pip3 install --user --no-cache-dir guake
 
+# install curl
+sudo apt install -yqqq curl
+
+# add .desktop entries
+curl -fsSL https://raw.githubusercontent.com/DevInsideYou/install-guake/master/guake-prefs.desktop -o ~/.local/share/applications/guake-prefs.desktop
+
+curl -fsSL https://raw.githubusercontent.com/DevInsideYou/install-guake/master/guake.desktop -o ~/.local/share/applications/guake.desktop
+
 echo
 echo '"guake" is now on the path but you might need to log out and back in'
